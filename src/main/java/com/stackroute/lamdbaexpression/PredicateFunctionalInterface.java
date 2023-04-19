12d934005ece3754ec5910ad50cbaf5a4d1fb8b9
Shivkumar Thakorbhai Patel <P7113613@ntil.com>
2023-04-19 16:08:20 +0530

    Solved

--- src/main/java/com/stackroute/lamdbaexpression/PredicateFunctionalInterface.java
+++ src/main/java/com/stackroute/lamdbaexpression/PredicateFunctionalInterface.java
@@ -1,10 +1,21 @@
 package com.stackroute.lamdbaexpression;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PredicateFunctionalInterface {
     //write logic to find the values that starts with letter I in the given list
     public List<String> findPattern(List<String> list) {
-        return null;
-    }
-}
+    	  List<String> startsWithI= new ArrayList<>(); 
+    	     list.forEach((li)->
+    	     {
+    	      if(li.startsWith("I")){
+    	       if(!startsWithI.contains(li)) {
+    	    	   startsWithI.add(li);
+
+    	       }
+    	      }
+    	     });
+    	        return startsWithI;
+    	    }
+    	}
